092653059805e2553a5c370409982d337becf872
AsadbekFF <79692640+AsadbekFF@users.noreply.github.com>
2021-09-25 18:49:28 +0500

    Initial commit

---
+++ Task1/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+            try
+            {
+                Product temp = (Product)obj;
+                if (Name == temp.Name && Price == temp.Price)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
